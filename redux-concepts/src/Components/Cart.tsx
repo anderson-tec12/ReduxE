e81eaf554841ecbe98acaa05e455855c0a565088
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-11-14 17:29:40 -0300

    redux typing

--- redux-concepts/src/Components/Cart.tsx
+++ redux-concepts/src/Components/Cart.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import {useSelector} from 'react-redux';
+import {IState} from '../store';
+import {ICartItem} from '../store/modules/cart/types';
+
 export default function Cart() {
-  const state = useSelector(state => state)
+  const cart = useSelector<IState, ICartItem[] >(state => state.cart.items)
+
   return (
     <div>
       <table>
@@ -14,7 +18,16 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-
+          {
+            cart.map(item => (
+              <tr key={item.product.id}>
+                <td>{item.product.title}</td>
+                <td>{item.product.price}</td>               
+                <td>{item.quantity}</td>               
+                <td>{(item.quantity * item.product.price).toFixed(2)}</td>               
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </div>
--- redux-concepts/src/store/index.ts
+++ redux-concepts/src/store/index.ts
@@ -1,5 +1,11 @@
 import {createStore} from 'redux';
 import rootReducers from './modules/rootReducers'
+import {ICartState} from './modules/cart/types';
+
+export interface IState{
+  cart: ICartState
+}
+
 //toda a aplicação tem acesso ao store
 const store = createStore(rootReducers)
 
