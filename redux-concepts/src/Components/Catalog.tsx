501ac25365bd77874701bcc6d3cd6d973c6caf7e
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-11-14 16:33:03 -0300

    get products and list itens

--- redux-concepts/src/Components/Catalog.tsx
+++ redux-concepts/src/Components/Catalog.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useStore, useSelector} from 'react-redux';
-
+import {IProduct} from '../store/modules/cart/types'
+import api from '../services/api'
 // import { Container } from './styles';
 
 const Components: React.FC = () => {
-  const store = useStore()
-  const state = useSelector(state => state)
-
+  const [catalog, setCatalog] = useState<IProduct[]>([])
   
-  console.log('NÂO RECOMENDADO Acesso ao STORE', store)
-  console.log('NÂO RECOMENDADO  Acesso ao estado da aplicação', store.getState())
+  useEffect(() => {
+    api.get('products').then(resp => {setCatalog(resp.data)})   
+  }, [])
 
-  console.log('SUPER RECOMENDADO  Acesso ao estado da aplicação', state)
+  {
+    const store = useStore()
+    const state = useSelector(state => state)
+
+    
+    console.log('NÂO RECOMENDADO Acesso ao STORE', store)
+    console.log('NÂO RECOMENDADO  Acesso ao estado da aplicação', store.getState())
+
+    console.log('SUPER RECOMENDADO  Acesso ao estado da aplicação', state)
+  }
   return (
-    <h1>
-      Catalog
-    </h1>
+    <>
+      <h1>
+        Catalog
+      </h1>
+
+      {
+        catalog.map(product => (
+          <article key={product.id}>
+            <strong>{product.title}</strong> {" - "}
+            <span>{product.price}</span> {"  "}
+            <button type="button">Comprar</button>
+          </article>
+        ))
+      }
+    </>
   );
 }
 
