769b1e876824aeee1fa27834ee8c96980f76cb17
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-11-14 18:00:21 -0300

    debuggingi with devtools

--- redux-concepts/src/store/index.ts
+++ redux-concepts/src/store/index.ts
@@ -1,12 +1,13 @@
 import {createStore} from 'redux';
 import rootReducers from './modules/rootReducers'
 import {ICartState} from './modules/cart/types';
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 export interface IState{
   cart: ICartState
 }
 
 //toda a aplicação tem acesso ao store
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, composeWithDevTools())
 
 export default store
