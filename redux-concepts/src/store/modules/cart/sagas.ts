03473df3be684e610008d19b239d5f27a94cdb5b
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-11-17 10:23:00 -0300

    getting the cart quantity

--- redux-concepts/src/store/modules/cart/sagas.ts
+++ redux-concepts/src/store/modules/cart/sagas.ts
@@ -1,9 +1,23 @@
-import {all, takeLatest} from  'redux-saga/effects';
+import {all, takeLatest, select} from  'redux-saga/effects';
+import {addProductToCart} from './actions'
+import {IState} from '../../'
  /*
   takeLatest = executaso a ultima
+  select = busca informações do estado global
  */
-function checkProductStock(){
+
+ type CheckProductRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock({payload}:CheckProductRequest){
+  console.log('Toda funcao recebe uma Action', payload)
   console.log('Adicionou ao carrinho')
+
+  const {product} = payload;
+  const currentQuantity:number = yield  select((state:IState)=> {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+  })
+
+  console.log('currentQuantity', currentQuantity)
 }
 
 export default all([
